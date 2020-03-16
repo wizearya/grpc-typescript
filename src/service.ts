c0e5d97836342e07ebbdbb2125a23de42bc63526
wizearya <arya@wize-net.co.jp>
2020-03-16 14:30:57 +0900

    test api response

--- src/service.ts
+++ src/service.ts
@@ -1,21 +1,22 @@
 import { ServerUnaryCall } from 'grpc';
-import {UsersRequest, UsersReply, User} from './protos/service_pb';
+import {UsersRequest, UsersReply} from './protos/service_pb';
 // import { findUsers } from './repository'
 import { status } from 'grpc';
+import { findUsers } from './repository';
 /**
  * getUsers
  * @param {any} call
  * @param {ServerUnaryCall<UsersRequest>} callback
  */
 export async function getUsers(call: ServerUnaryCall<UsersRequest>, callback: any): Promise<void> {
-  // const request: UsersRequest = call.request;
+   const request: UsersRequest = call.request;
   console.log(call.metadata.getMap());
 
   const reply: UsersReply = new UsersReply();
 
   try {
-    // reply.setUsersList(await findUsers(request.getResultcount()));
-    reply.setUsersList([dummy()]);
+    reply.setUsersList(await findUsers(request.getResultcount()));
+    // reply.setUsersList([dummy()]);
     callback(null, reply);
 
   } catch (e) {
@@ -29,7 +30,7 @@ export async function getUsers(call: ServerUnaryCall<UsersRequest>, callback: an
 /**
  * Temporary processing for load test ...
  */
-function dummy(): User {
+/*function dummy(): User {
   const user: User = new User();
   user.setFirstname('dummy');
   user.setLastname('dummy');
@@ -37,4 +38,4 @@ function dummy(): User {
   user.setEmail('dummy@sample.com');
 
   return user;
-}
+}*/
