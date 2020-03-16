f44a1f2ccbb4d9aa95e6be3593bb30159ec51ee1
wizearya <arya@wize-net.co.jp>
2020-03-16 10:41:58 +0900

    delete

--- sample-loadtest.sh
+++
@@ -1,8 +0,0 @@
-ghz --insecure \
-  --proto ./protos/service.proto \
-  --call test_user.UserDomain.getUsers \
-  -d '{"resultCount": 1}' \
-  --connections 50 \
-  -c 50 \
-  -n 1000 \
-  0.0.0.0:50051
